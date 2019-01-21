1191b226541cdac07b8b6f23c195e82bba3c6a22
Christopher Donnelly <christopherdonnelly@christophers-macbook.local>
2019-01-21 15:34:28 -0500

    2.5 Challenge Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     //linked label to code
     @IBOutlet weak var messageLabel: UILabel!
+    // create index
+//    var index = 0
     
     //Code below executes when the app view first loads
     override func viewDidLoad() {
@@ -20,15 +22,44 @@ class ViewController: UIViewController {
 
     }
     //linked button to code
-    @IBAction func showMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You are Awesome!"
-        messageLabel.textColor = UIColor.magenta
-        
-        
-    }
     @IBAction func showAnotherMessage(_ sender: UIButton) {
         messageLabel.text = "You are Great!"
-        messageLabel.textColor = UIColor.blue
     }
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are Amazing!"
+        
+//        let messages = ["You are Fantastic!!", "You are Great!", "You are Amazing"]
+//
+//        messageLabel.text = messages[index]
+//        if index < messages.count-1 {
+//            index += 1
+//        } else if index == messages.count-1 {
+//            index = 0
+//        }
+        
+        
+//        if messageLabel.text == messages[0] {
+//            messageLabel.text = messages[1]
+//        } else if messageLabel.text == messages[1] {
+//            messageLabel.text = messages[2]
+//        } else {messageLabel.text = messages[0]
+//
+//        }
+        
+
+//        let message1 = "You are Awesome!"
+//        let message2 = "You are Great!"
+//        let message3 = "You are Amazing!"
+//
+//        if messageLabel.text == message1 {
+//           messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//           messageLabel.text = message3
+//        } else {
+//           messageLabel.text = message1
+//        }
+   }
+    
 }
+
 
